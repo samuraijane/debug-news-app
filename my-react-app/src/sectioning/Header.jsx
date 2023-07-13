0401cc75a156bf9d2d0c127876870a084478db96
Hodan Suleiman <suleimanhodan9@gmail.com>
2023-07-13 19:51:08 -0400

    header changes

--- my-react-app/src/sectioning/Header.jsx
+++ my-react-app/src/sectioning/Header.jsx
@@ -70,26 +70,8 @@ const DropdownLink = styled.a`
   &:hover {
     text-decoration: underline;
   }
-`;
 
-  padding-right: 100px;
-`;
 
-const NavLink = styled(Link)`
-  color: #fff;
-  text-decoration: none;
-  margin-right: 15px;
-  font-size: 14px;
-
-  &:last-child {
-    margin-right: 0;
-  }
-`;
-const Logo = styled.img`
- width: 100px;
- height: 75px;
- padding-bottom: 20px;
-`
 
 
 const Header = () => {
@@ -134,21 +116,6 @@ const Header = () => {
             <DropdownLink href="/account">Account</DropdownLink>
           </DropdownMenu>
         )}
-
-        <Logo src="/images/customlogo.JPG" alt="Logo" />
-        <div>
-          <NavLink to="/headlines">Headlines</NavLink>
-          <NavLink to="/world">World</NavLink>
-          <NavLink to="/politics">Politics</NavLink>
-          <NavLink to="/business">Business</NavLink>
-          <NavLink to="/health">Health</NavLink>
-          <NavLink to="/entertainment">Entertainment</NavLink>
-          <NavLink to="/travel">Travel</NavLink>
-          <NavLink to="/sports">Sports</NavLink>
-          <NavLink to="/profile">Profile</NavLink>
-          <NavLink to="/account">Account</NavLink>
-          </div>
-        </Nav>
 
       </div>
     </HeaderContainer>
