7c65155a4215ba3835a34fc7e4fe562df2beb637
Hodan Suleiman <suleimanhodan9@gmail.com>
2023-07-18 21:04:31 -0400

    worked on session, mongodb

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,7 +3,8 @@ const checkAuth = (req, res, next) => {
     if (req.session.user) {
       next();
     } else {
-      res.redirect('/login');
+      res.redirect('/login'); // page or route
+    
     }
   };
 
